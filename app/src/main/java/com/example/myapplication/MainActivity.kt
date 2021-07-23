c7f571970ea78b433723dc8fccc735901f3ff5d5
Go-yj <71641753+Go-yj@users.noreply.github.com>
2021-07-23 23:27:28 +0900

    시작화면, 계좌 확인창

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login_button.setOnClickListener {
+            val intent = Intent(this, Registration1::class.java)
+            startActivity(intent)
+        }
+
+        account_check_button.setOnClickListener {
+            Toast.makeText(this, "계좌 인증 버튼 클릭", Toast.LENGTH_SHORT).show()
+       }
     }
 }
